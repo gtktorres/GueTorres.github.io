a9812d887518c72306e329e2baee1d2f9a3dcd42
Guevara Torres <guevara.torres619@outlook.com>
2019-10-01 11:45:00 -0400

    Name, portrait, background and about me aligned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 import Me from './boston.PNG';
 
 export default class App extends React.Component {
+  
   render() {
 
   return (
-
-
-
     <div className="App">
-
-      <h1>Guevara Torres</h1>
-
-      <img src={Me} className="App-port" alt="logo" />
-      <div className="Card">
+      <h1 className="Header">Guevara Torres</h1>
+      <img src={Me} className="App-port" alt="logo" />      
+    <div className="Card">
         <p className="About-Me"> I'm a senior at Lehman college pursuing a Bachelor of Science in Computer Science. With an interest in mobile applications as well as Web applicatons.</p>
-      </div>
-
+    </div>
     </div>
   );
   }
