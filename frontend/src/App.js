435bfc9d4900986e2e53bdecc1f4e2c3bb937018
Guevara Torres <guevara.torres619@outlook.com>
2019-10-01 13:28:28 -0400

    created more css files for generic border settings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,12 @@ export default class App extends React.Component {
 
   return (
     <div className="App">
-      <h1 className="Header">Guevara Torres</h1>
-      <img src={Me} className="App-port" alt="logo" />      
+      <div className="Border">
+        <h1 className="Header">Guevara Torres</h1>
+        <img src={Me} className="App-port" alt="logo" />      
+      </div>
     <div className="Card">
+        <h2>About Me</h2>
         <p className="About-Me"> I'm a senior at Lehman college pursuing a Bachelor of Science in Computer Science. With an interest in mobile applications as well as Web applicatons.</p>
     </div>
     </div>
